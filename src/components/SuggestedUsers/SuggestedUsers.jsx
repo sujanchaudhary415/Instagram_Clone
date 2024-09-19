d4c69efda7edaf39e8c8ed5ab8d6375032332f59
sujanchaudhary415 <108916550+sujanchaudhary415@users.noreply.github.com>
2024-09-19 22:14:51 +0545

    Update SuggestedUsers.jsx

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -29,9 +29,9 @@ const SuggestedUsers = () => {
 			))}
 
 			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-				© 2023 Built By{" "}
+				© 2024 Built By{" "}
 				<Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color='blue.500' fontSize={14}>
-					As a Programmer
+					Sujan Chaudhary
 				</Link>
 			</Box>
 		</VStack>
